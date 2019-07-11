8218640f0c7f3ca21e3a64e0a4ee4117e10e2a25
messickr <rnmessick@gmail.com>
2019-07-11 09:36:52 -0700

    adding new repo

---
+++ README.md
@@ -0,0 +1,127 @@
+**Author**: Brandon Hurrington, Chris Coulon, Joachen Busch
+**Version**: 1.0.0 (increment the patch/fix version number if you make more commits past your first submission)
+
+## Overview
+Create an app to generate lat and long for a specific location as defined by name by the user.  The app should also have the ability to look up the weather for that location.  We also would like to incorporate an error detection function.
+
+## Getting Started
+The user should only have to open up the app on the internet and enter the name of a location.  Our app will then return lat and long, as well as weather data or inform the user that they have entered invalid information.
+
+## Architecture
+- JavaScript, JQuery
+- API: Google geocoding and DarkSky
+- Heroku
+
+## Change Log
+
+07-09-2019 1000am - Initial repo scaffolding complete.
+
+07-09-2019 1145am - Application now has a fully-functional express server, with a GET route for the location resource and will display map on page.
+
+07-09-2019 1300am - Application now has a fully-functional express server, with a GET route for the weather resource and will display map on page.
+
+## Credits and Collaborations
+Day 1 Help Collaborators: Trevor Dobson and Marisha Hoza
+Day 1 Team Credits: Brandon Hurrington, Chris Coulon, Joachen Busch
+
+Day 2 Help Collaborators: 
+Day 2 Team Credits: Brandon Hurrington, Renee Messick
+
+
+# Lab-06
+Number and name of feature: Feature 1: repo setup
+
+Estimate of time needed to complete: 10mins
+
+Start time: 9:30a
+
+Finish time: 10:00p
+
+Actual time needed to complete: 30mins
+
+Driver: Chris
+Navigator: Joachen, Brandon
+
+# Lab-06
+Number and name of feature: Feature 2: Locations
+
+Estimate of time needed to complete: 45mins
+
+Start time: 10:20a
+
+Finish time: 11:45a
+
+Actual time needed to complete: 1h 25mins
+
+Driver: Joachen
+Navigator: Chris, Brandon
+
+# Lab-06
+Number and name of feature: Feature 3: Weather
+
+Estimate of time needed to complete: 30mins
+
+Start time: 11:45a
+
+Finish time: 1:00p
+
+Actual time needed to complete: 1h 15min
+
+Driver: Brandon
+Navigator: Chris, Joachen
+
+# Lab-07
+Number and name of feature: Code Review
+
+Estimate of time needed to complete: 60 minutes
+
+Start time: 10:15am
+
+Finish time: 11:15am
+
+Actual time needed to complete: 60 minutes
+
+Driver: Renee
+Navigator: Brandon
+
+# Lab-07
+Number and name of feature: Heroku
+
+Estimate of time needed to complete: 30 minutes
+
+Start time: 11:15am
+
+Finish time: 12:00pm
+
+Actual time needed to complete:  45 minutes
+
+Driver: Brandon
+Navigator: Renee
+
+# Lab-07
+Number and name of feature: Feature 1: data formatting
+
+Estimate of time needed to complete: 30 minutes
+
+Start time: 12:0pm
+
+Finish time: 12:10pm
+
+Actual time needed to complete:  10 minutes
+
+Driver: Brandon
+Navigator: Renee
+
+# Lab-07
+Number and name of feature: Feature 2: weather formatting
+
+Estimate of time needed to complete: 30 minutes
+
+Start time: 12:0pm
+
+Finish time: 12:10pm
+
+Actual time needed to complete:  10 minutes
+
+Driver: Brandon
+Navigator: Renee
---
+++ server.js
@@ -0,0 +1,103 @@
+// APP dependencies
+require('dotenv').config();
+
+const express = require('express');
+const cors = require('cors');
+
+// Global variables
+const PORT = process.env.PORT;
+const GEOCODE_API_KEY = process.env.GEOCODE_API_KEY;
+const DARKSKY_API_KEY = process.env.DARKSKY_API_KEY;
+const EVENTBRITE_API_KEY = process.env.EVENTBRITE_API_KEY;
+
+// Construct server with dependency objects
+const app = express();
+app.use(cors());
+const superagent = require('superagent');
+
+// Use express to get location data
+app.get('/location', searchToLatLng);
+
+// Performs task of building object from JSON file
+function searchToLatLng(request, response) {
+  const locationName = request.query.data;
+  const geocodeURL = `https://maps.googleapis.com/maps/api/geocode/json?address=${locationName}&key=${GEOCODE_API_KEY}`;
+
+  superagent.get(geocodeURL)
+    .then(result => {
+      let location = new LocationConstructor(result.body);
+      location.search_query = locationName;
+
+      response.send(location);
+    }).catch(error => {
+      console.error(error);
+      response.status(500).send('Status 500: Life is hard mang.');
+    })
+}
+
+// constructor function to build weather objects
+function LocationConstructor(geoData) {
+  this.formatted_query = geoData.results[0].formatted_address;
+  this.latitude = geoData.results[0].geometry.location.lat;
+  this.longitude = geoData.results[0].geometry.location.lng;
+
+}
+
+
+// Use express to get weather data
+app.get('/weather', searchWeather);
+
+function searchWeather(request, response) {
+  const darkskyURL = `https://api.darksky.net/forecast/${DARKSKY_API_KEY}/${request.query.data.latitude},${request.query.data.longitude}`;
+  console.log(darkskyURL);
+  superagent.get(darkskyURL)
+    .then(result => {
+      let weather = result.body.daily.data.map( element => new WeatherConstructor(element));
+      response.send(weather);
+    }).catch(error => {
+      console.error(error);
+      response.status(500).send('Status 500: Life is hard mang.');
+    })
+}
+
+// constructor function to build weather objects
+function WeatherConstructor(element) {
+  this.forecast = element.summary,
+  this.time = new Date(element.time * 1000).toDateString()
+}
+
+//Search Eventbrite
+
+app.get('/events',searchEventbrite);
+
+function searchEventbrite(request,response){
+  const eventBriteURL = `https://www.eventbriteapi.com/v3/events/search/?location.longitude=${request.query.data.longitude}&location.latitude=${request.query.data.latitude}&expand=venue&token=${EVENTBRITE_API_KEY}`;
+
+  superagent.get(eventBriteURL)
+    .then(result => {
+      let eventData = result.body.events.map( event => new EventConstructor(event.url, event.name.text, event.start.local, event.summary));
+      // console.log(eventData);
+      response.send(eventData);
+    }).catch(error => {
+      console.error(error);
+      response.status(500).send('Status 500: Sadly, Events are not working');
+    });
+}
+
+function EventConstructor(link, name, event_date, summary){
+  this.link = link;
+  this.name = name;
+  this.event_date = new Date(event_date).toDateString();
+  this.summary = summary;
+
+}
+
+// error handling
+app.use('*', (request, response) => {
+  response.send('you got to the wrong place');
+})
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`app is up on port ${PORT}`)
+})
